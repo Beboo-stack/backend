2d6fcca6cfd438f6da33f1ae94e8c8e268d34291
Beboo-stack <peteralber735@gmail.com>
2024-10-24 04:32:18 +0300

    end

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,39 @@
-module.exports = [
-  "strapi::logger",
-  "strapi::errors",
+// File: config/env/production/middlewares.js
+
+module.exports = ({ env }) => [
+  'strapi::logger',
+  'strapi::errors',
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('S3_BUCKET') + '.' + env('S3_ENDPOINT'),
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('S3_BUCKET') + '.' + env('S3_ENDPOINT'),
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+]
