d8d5eb4aa181da59ef6ce17535a93f8230c94965
Beboo-stack <peteralber735@gmail.com>
2024-10-24 03:07:24 +0300

    keys

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 5432),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 5432),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["api-key-2", "api-key"]),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
